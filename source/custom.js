6ebf4834f5b9c5849ecd18b6ae074c9f98610b8e
Jat <jat@sinosky.org>
2023-09-05 20:43:53 +0800

    v0.3.0
    Signed-off-by: Jat <jat@sinosky.org>

--- source/custom.js
+++
@@ -1,7 +0,0 @@
-import('//cdn.jsdelivr.net/npm/isbot/index.min.mjs').then((Module) => {
-    const isbot = Module.default;
-
-    if (!isbot(navigator.userAgent)) {
-        window.location = 'https://github.com/jat001';
-    }
-})
