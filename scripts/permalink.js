2b4d38abef2e7625578aa853c5e361eb0e93b8b4
Jat <jat@sinosky.org>
2023-11-06 23:45:32 +0800

    update permalink
    Signed-off-by: Jat <jat@sinosky.org>

--- scripts/permalink.js
+++ scripts/permalink.js
@@ -1,7 +1,12 @@
 hexo.extend.filter.register('post_permalink', function (permalink) {
-  if (RegExp('^20\\d{2}/').test(permalink) && Number(permalink.slice(0, 4)) < 2023)
-    permalink = permalink.slice(5);
-  if (permalink.startsWith('archive-'))
-    permalink = permalink.slice(8);
-  return permalink;
-});
+  const res = RegExp('^(20\\d{2})\\d{4}/(archive-)?').exec(permalink)
+
+  if (res !== null) {
+    if (res[1] < 2023) {
+      permalink = permalink.slice(9)
+      if (res[2] !== undefined) permalink = permalink.slice(8)
+    } else permalink = permalink.slice(0, 8)
+  }
+
+  return permalink
+})
